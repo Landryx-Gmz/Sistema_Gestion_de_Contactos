755b730e67407f0c6fc1fd4b445909f43af476b7
Andy Gomez <153938897+Landryx-Gmz@users.noreply.github.com>
2025-04-23 08:34:17 +0200

    clase_GestionContactos.py_creada

--- src/gestion_contactos.py
+++ src/gestion_contactos.py
@@ -1,7 +1,23 @@
+import os
+
+
 class GestioContactos:
     def __init__(self):
         self.nombre_archivo = 'contactos.txt'
 
     def agregar_contactos(self, contacto):
-        with open(self.nombre_archivo, 'a', encoding='utf8') as archivo:
-            archivo.write(f'{contacto.nombre},{contacto.num_telef},{contacto.email}')
+        try:
+            with open(self.nombre_archivo, 'a', encoding='utf8') as archivo:
+                archivo.write(f'{contacto.nombre},{contacto.num_telef},{contacto.email}')
+                print('El contacto se agrego correctamente')
+        except Exception as e:
+            print(f'Error al agregar contacto {e}')
+
+    def lista_contactos(self):
+        with open(self.nombre_archivo, 'r', encoding='utf8') as archivo:
+            print('----Contactos----')
+            archivo.read()
+
+    def eliminar_lista_contactos(self):
+        os.remove(self.nombre_archivo)
+        print('Lista eliminada correctamente')
